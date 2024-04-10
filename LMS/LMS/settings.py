c2793b03102eadf1cb33cfe769785894b5f09061
Rupesh Nepal <rupeshrnepal@gmail.com>
2024-04-10 17:00:36 +0545

    created postgresssql env for vercel

--- LMS/LMS/settings.py
+++ LMS/LMS/settings.py
@@ -124,8 +124,12 @@ env.read_env()
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', 
-        'NAME': BASE_DIR / 'db.sqlite3', 
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
 
